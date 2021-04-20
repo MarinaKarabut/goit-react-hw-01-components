49b0588b95c72212e954f52f7a17ed4001943959
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-04-20 03:40:13 +0300

    react-homework-01

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import styles from './Statistics.module.css';
+
+
+
+
+function Statistics({title, stats}) {
+    const statElements = stats.map(({id, label, percentage}) => (<li className={styles.item} key={id}>
+        <span className={styles.label}>{label}</span>
+        <span className={styles.percentage}>{percentage}%</span>
+        </li>))
+        
+    return (
+    <section className={styles.statistics}>
+        {title && <h2 className={styles.title}>{title}</h2>}
+        <ul className={styles.statList}>
+            {statElements}
+        </ul>
+    </section>
+    )
+};
+
+export default Statistics;
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+        PropTypes.shape(
+        {
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+        })
+    ).isRequired,
+  }
+  
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import styles from './TransactionHistory.module.css';
+
+
+function TransactionHistory({items}) {
+    const transactionElements = items.map(({id, type, amount, currency}) => (
+        <tr key= {id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+        </tr>
+    ))
+    return (
+        <section className ="transactions">
+            <table className={styles.transactionHistory}>
+                <thead className={styles.thead}>
+                    <tr>
+                        <th>Type</th>
+                        <th>Amount</th>
+                        <th>Currency</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactionElements}
+                </tbody>
+            </table>
+        </section>
+    )
+};
+
+export default TransactionHistory;
+
+TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape(
+        {
+            id: PropTypes.string.isRequired,
+            type: PropTypes.string.isRequired,
+            amount: PropTypes.string.isRequired,
+            currency: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+  }
+  
+
+
